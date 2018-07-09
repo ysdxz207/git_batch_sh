970a25e932526cdf43c3f32747c6386b43357d9a
ysdxz207 <ysdxz207@qq.com>
2018-07-09 10:16:25 +0800

    Update and rename git_batch_sh.sh to git_batch.sh

--- git_batch.sh
+++ git_batch.sh
@@ -1,7 +1,7 @@
 #批量git操作v2
 #Author:Moses
-#使用方法：sh git_batch_sh.sh git checkout branchname
-#报错不终止，继续执行：sh git_batch_sh.sh -f git checkout branchname
+#使用方法：sh git_batch.sh git checkout branchname
+#报错不终止，继续执行：sh git_batch.sh -f git checkout branchname
 
 force=0
 if [ "$1" == "-f" ]
